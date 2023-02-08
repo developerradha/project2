686fef9cc925e5bd358cc393e1c095654daf5879
developerradha <118384403+developerradha@users.noreply.github.com>
2023-02-08 19:42:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbvzc1ksYLPkxWMePz4QEwuqHN5W9GSNLw3Q&usqp=CAU"
+    div.appendChild(image);
+}
